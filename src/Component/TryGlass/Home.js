9f5987fa967e072355599e9e799f2867827a0641
Huy Nam <namhh33@gmail.com>
2023-01-04 21:08:14 +0700

    completed

--- src/Component/TryGlass/Home.js
+++ src/Component/TryGlass/Home.js
@@ -70,31 +70,22 @@ export default class Home extends Component {
   ];
 
   state = {
-    glassImage: "./glassesImage/v3.png",
+    glassItem: null,
   };
 
   changeGlass(glass) {
     this.setState({
-      glassImage: glass,
+      glassItem: glass,
     });
   }
 
-  // renderGlass() {
-  //   return this.glassList.map((glass) => {
-  //     return (
-  //       <div className="test-model">
-  //         <img className="img1" src="./glassesImage/model.jpg" alt="model" />
-  //         <div className="testGlass">
-  //           <img src={this.state.glassImage} alt="test1" />
-  //         </div>
-  //         <div className="text">
-  //           <h3>{glass.name}</h3>
-  //           <p>{glass.desc}</p>
-  //         </div>
-  //       </div>
-  //     );
-  //   });
-  // }
+  renderGlassImage() {
+    return this.glassList.map((glass) => (
+      <button key={glass.id} onClick={() => this.changeGlass(glass)}>
+        <img src={glass.url} alt="g1" />
+      </button>
+    ));
+  }
 
   render() {
     return (
@@ -105,22 +96,24 @@ export default class Home extends Component {
         <div className="container">
           <div className="model-image">
             <div className="test-image">
-              {/* {this.renderGlass()} */}
               <div className="test-model">
                 <img
                   className="img1"
                   src="./glassesImage/model.jpg"
                   alt="model"
                 />
-                <div className="testGlass">
-                  <img src={this.state.glassImage} alt="test1" />
-                </div>
-                <div className="text">
-                  <h3>GUCCI G8850U</h3>
-                  <p>
-                    Light pink square lenses define these sunglasses, ending
-                    with amother of pearl effect tip.{" "}
-                  </p>
+                <div>
+                  {this.state.glassItem && (
+                    <div className="testGlass">
+                      <img src={this.state.glassItem.url} alt="test1" />
+                    </div>
+                  )}
+                  {this.state.glassItem && (
+                    <div className="text">
+                      <h3>{this.state.glassItem.name}</h3>
+                      <p>{this.state.glassItem.desc}</p>
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
@@ -134,57 +127,7 @@ export default class Home extends Component {
             </div>
           </div>
 
-          <div className="products">
-            <button
-              onClick={() => {
-                this.changeGlass("./glassesImage/v1.png");
-              }}
-            >
-              <img src="./glassesImage/g1.jpg" alt="g1" />
-            </button>
-            <button
-              onClick={() => {
-                this.changeGlass("./glassesImage/v2.png");
-              }}
-            >
-              <img src="./glassesImage/g2.jpg" alt="g2" />
-            </button>
-            <button
-              onClick={() => {
-                this.changeGlass("./glassesImage/v3.png");
-              }}
-            >
-              <img src="./glassesImage/g3.jpg" alt="g3" />
-            </button>
-            <button
-              onClick={() => {
-                this.changeGlass("./glassesImage/v4.png");
-              }}
-            >
-              <img src="./glassesImage/g4.jpg" alt="g4" />
-            </button>
-            <button
-              onClick={() => {
-                this.changeGlass("./glassesImage/v5.png");
-              }}
-            >
-              <img src="./glassesImage/g5.jpg" alt="g5" />
-            </button>
-            <button
-              onClick={() => {
-                this.changeGlass("./glassesImage/v6.png");
-              }}
-            >
-              <img src="./glassesImage/g6.jpg" alt="g6" />
-            </button>
-            <button
-              onClick={() => {
-                this.changeGlass("./glassesImage/v7.png");
-              }}
-            >
-              <img src="./glassesImage/g7.jpg" alt="g7" />
-            </button>
-          </div>
+          <div className="products">{this.renderGlassImage()}</div>
         </div>
       </div>
     );
