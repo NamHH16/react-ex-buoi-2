50bb528005f815e4f7f393ec8535d57999e14c79
Huy Nam <namhh33@gmail.com>
2023-01-04 20:29:39 +0700

    change glass

--- src/Component/TryGlass/Home.js
+++ src/Component/TryGlass/Home.js
@@ -3,8 +3,74 @@ import "./Home";
 import "./Home.css";
 
 export default class Home extends Component {
+  glassList = [
+    {
+      id: 1,
+      price: 30,
+      name: "GUCCI G8850U",
+      url: "./glassesImage/v1.png",
+      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
+    },
+    {
+      id: 2,
+      price: 50,
+      name: "GUCCI G8759H",
+      url: "./glassesImage/v2.png",
+      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
+    },
+    {
+      id: 3,
+      price: 30,
+      name: "DIOR D6700HQ",
+      url: "./glassesImage/v3.png",
+      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
+    },
+    {
+      id: 4,
+      price: 70,
+      name: "DIOR D6005U",
+      url: "./glassesImage/v4.png",
+      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
+    },
+    {
+      id: 5,
+      price: 40,
+      name: "PRADA P8750",
+      url: "./glassesImage/v5.png",
+      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
+    },
+    {
+      id: 6,
+      price: 60,
+      name: "PRADA P9700",
+      url: "./glassesImage/v6.png",
+      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
+    },
+    {
+      id: 7,
+      price: 80,
+      name: "FENDI F8750",
+      url: "./glassesImage/v7.png",
+      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
+    },
+    {
+      id: 8,
+      price: 100,
+      name: "FENDI F8500",
+      url: "./glassesImage/v8.png",
+      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
+    },
+    {
+      id: 9,
+      price: 60,
+      name: "FENDI F4300",
+      url: "./glassesImage/v9.png",
+      desc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. ",
+    },
+  ];
+
   state = {
-    glassImage: "./glassesImage/v1.png",
+    glassImage: "./glassesImage/v3.png",
   };
 
   changeGlass(glass) {
@@ -12,6 +78,23 @@ export default class Home extends Component {
       glassImage: glass,
     });
   }
+
+  // renderGlass() {
+  //   return this.glassList.map((glass) => {
+  //     return (
+  //       <div className="test-model">
+  //         <img className="img1" src="./glassesImage/model.jpg" alt="model" />
+  //         <div className="testGlass">
+  //           <img src={this.state.glassImage} alt="test1" />
+  //         </div>
+  //         <div className="text">
+  //           <h3>{glass.name}</h3>
+  //           <p>{glass.desc}</p>
+  //         </div>
+  //       </div>
+  //     );
+  //   });
+  // }
 
   render() {
     return (
@@ -22,6 +105,7 @@ export default class Home extends Component {
         <div className="container">
           <div className="model-image">
             <div className="test-image">
+              {/* {this.renderGlass()} */}
               <div className="test-model">
                 <img
                   className="img1"
@@ -51,25 +135,53 @@ export default class Home extends Component {
           </div>
 
           <div className="products">
-            <button onClick={this.changeGlass("./glassesImage/v1.png")}>
+            <button
+              onClick={() => {
+                this.changeGlass("./glassesImage/v1.png");
+              }}
+            >
               <img src="./glassesImage/g1.jpg" alt="g1" />
             </button>
-            <button onClick={this.changeGlass("./glassesImage/v2.png")}>
+            <button
+              onClick={() => {
+                this.changeGlass("./glassesImage/v2.png");
+              }}
+            >
               <img src="./glassesImage/g2.jpg" alt="g2" />
             </button>
-            <button onClick={this.changeGlass("./glassesImage/v3.png")}>
+            <button
+              onClick={() => {
+                this.changeGlass("./glassesImage/v3.png");
+              }}
+            >
               <img src="./glassesImage/g3.jpg" alt="g3" />
             </button>
-            <button onClick={this.changeGlass("./glassesImage/v4.png")}>
+            <button
+              onClick={() => {
+                this.changeGlass("./glassesImage/v4.png");
+              }}
+            >
               <img src="./glassesImage/g4.jpg" alt="g4" />
             </button>
-            <button onClick={this.changeGlass("./glassesImage/v5.png")}>
+            <button
+              onClick={() => {
+                this.changeGlass("./glassesImage/v5.png");
+              }}
+            >
               <img src="./glassesImage/g5.jpg" alt="g5" />
             </button>
-            <button onClick={this.changeGlass("./glassesImage/v6.png")}>
+            <button
+              onClick={() => {
+                this.changeGlass("./glassesImage/v6.png");
+              }}
+            >
               <img src="./glassesImage/g6.jpg" alt="g6" />
             </button>
-            <button onClick={this.changeGlass("./glassesImage/v7.png")}>
+            <button
+              onClick={() => {
+                this.changeGlass("./glassesImage/v7.png");
+              }}
+            >
               <img src="./glassesImage/g7.jpg" alt="g7" />
             </button>
           </div>
